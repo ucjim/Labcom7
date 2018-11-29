bf3e7d8c025458d7a35e6b1dea0e54e9ac4520ee
jarudat chaikuad <60030014@kmitl.ac.th>
2018-11-30 01:31:12 +0700

    ok

--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -6,25 +6,25 @@ using System.Threading.Tasks;
 
 namespace Lab7
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Student su = new Student();
-            su.Name = "Student Name";
-            su.ID = "12345678";
-            su.GPA = 7.5f;
-            Console.WriteLine("Student name : " + su.Name);
-            Console.WriteLine("Student ID   : " + su.ID);
-            Console.WriteLine("Student GPA  : " + su.GPA);
-            Console.ReadLine();
-        }
-    }
     class Student
     {
-        private string Name;
-        private string ID;
-        private float GPA;
-
+        private string name;
+        private string id;
+        private float gpa;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public float GPA
+        {
+            get { return gpa; }
+            set { gpa = value; }
+        }
     }
 }
