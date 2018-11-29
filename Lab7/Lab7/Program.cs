41b70fcd3294162d25eaf503f42e6a310f061a36
jarudat chaikuad <60030014@kmitl.ac.th>
2018-11-30 01:25:16 +0700

    error

---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student su = new Student();
+            su.Name = "Student Name";
+            su.ID = "12345678";
+            su.GPA = 7.5f;
+            Console.WriteLine("Student name : " + su.Name);
+            Console.WriteLine("Student ID   : " + su.ID);
+            Console.WriteLine("Student GPA  : " + su.GPA);
+            Console.ReadLine();
+        }
+    }
+    class Student
+    {
+        private string Name;
+        private string ID;
+        private float GPA;
+
+    }
+}
